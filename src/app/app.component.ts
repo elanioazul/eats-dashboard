07271d54d8e5a06094a7f3328b240af289716c69
Hugo Herrador <hugo.herrador@geograma.com>
2023-03-08 15:47:40 +0100

    Dispach initial menu action

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { appLoaded } from './core/state/menus';
 
 @Component({
 	selector: 'app-root',
@@ -7,4 +9,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 	title = 'spa-angular-typescript-dashboard';
+
+	constructor(private store: Store) {}
+
+	ngOnInit(): void {
+		this.store.dispatch(appLoaded());
+	}
 }
